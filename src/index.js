c416627ebeb4fa2d817b4f2457f442fd693984b7
Caitlyn Fowkes <caitlynfowkes@gmail.com>
2018-06-10 17:50:01 +0200

    Adding VideoList section

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search'
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
-import VideoListItem from './components/video_list_item';
 
 const API_KEY = 'AIzaSyCkz5KuZN6CrU6zvclF4igDfPuGdZadsgU';
 
@@ -21,7 +20,7 @@ class App extends React.Component {
 	
 
 render () {
-
+console.log('props.video', this.state.videos)
 return ( <div>
 	<SearchBar />
 	<VideoList videos={this.state.videos} />
