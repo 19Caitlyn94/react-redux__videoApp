4558dabf43214931fabcf79ebf361d1693536fc0
Caitlyn Fowkes <caitlynfowkes@gmail.com>
2018-06-10 19:13:13 +0200

    Video detail section inside index

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search'
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail'
 
 const API_KEY = 'AIzaSyCkz5KuZN6CrU6zvclF4igDfPuGdZadsgU';
 
@@ -23,6 +24,7 @@ render () {
 console.log('props.video', this.state.videos)
 return ( <div>
 	<SearchBar />
+	<VideoDetail video={this.state.videos[0]} />
 	<VideoList videos={this.state.videos} />
 	</div>
 )
